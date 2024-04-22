ccc4028c79f9a5d801f30bb70db03747ef7f78ed
Alkoten <123296863+Alkoten@users.noreply.github.com>
2024-04-22 21:30:38 +0500

    22.04.24
    Добавлены инструменты управления кондиционером: "Вкл". "Выкл". "Авто"

--- Temperature/getData.php
+++ Temperature/getData.php
@@ -9,11 +9,31 @@ $dbname = "serg153e_temp";
 
 // Создание подключения
 $conn = new mysqli($servername, $username, $password, $dbname);
+////////////////////////////////////
+if(isset($_POST['action']) && $_POST['action'] == 'OFF') 
+    {
+        $query = "UPDATE `Status` SET `Status`= 0 WHERE ID = 1";
+        $result = mysqli_query($conn, $query); 
+    }
+
+else if (isset($_POST['action']) && $_POST['action'] == 'ON')
+     {
+        $query = "UPDATE `Status` SET `Status`= 1 WHERE ID = 1";
+        $result = mysqli_query($conn, $query); 
+    }
+else if (isset($_POST['action']) && $_POST['action'] == 'AUTO')
+     {
+        $query = "UPDATE `Status` SET `Status`= 2 WHERE ID = 1";
+        $result = mysqli_query($conn, $query); 
+    }
+
+/////////////////////////////
 
 // Проверка соединения
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 // Запрос данных из таблицы
 $sql = "SELECT Id, Temp, Stat FROM `Temperature` ORDER BY Id DESC LIMIT 1"; 
 
@@ -24,11 +44,17 @@ echo '<div style="text-align:center; color:red; font-size:55px;">';
 foreach ($result as $row) {
     echo '<b>'. $row['Temp']. '</b><br>'. '<br>';
 }
-    if ($row['Stat'] == 1) {
-        echo "<b>Вкл</b>";
+    if ($row['Stat'] == 0) {
+        echo "<b>Выкл</b>";
          
-    } else {
-        echo '<b>Выкл</b>';
+    } else if ($row['Stat'] == 1) {
+        echo '<b>Вкл</b>';
+        
+    }   else if ($row['Stat'] == 2) {
+        echo "<b>Авто Вкл</b>";
+         
+    } else if ($row['Stat'] == 3) {
+        echo '<b>Авто Выкл</b>';
         
     }
 
