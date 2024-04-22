2c1ba0d8499b89cb27664d57d462c5edc99cd06f
Сергей Лекомцев <serg15997@gmail.com>
2024-04-22 21:31:09 +0500

    22.04.24
    Добавлена обратная связь с сервером,  есть 3 статуса вкл выкл или авто
    авто делиться на 2 тип 2 и 3 что бы сервер понимал состояние кондера в авто режиме

--- rasbery/main.py
+++ rasbery/main.py
@@ -1,10 +1,11 @@
 import requests
 import time
 import datetime
+from bs4 import BeautifulSoup
 
 patchtemp='/home/user/Desktop/temp.txt'
 patchstat='/home/user/Desktop/stat.txt'
-
+url_chek='https://lekweb.store/chek.php'
 def read_file(patchtemp):
 	with open(patchtemp,'r') as file:
 		value=file.read().strip()
@@ -20,14 +21,41 @@ def save_temp_to_file(stat,patchstat):
 		file.write(str(stat))
 		file.close()
 
+# stat 0-off 1-on 2-autoOn 3-autoOff
+def chek_stat():
+	response = requests.get(url_chek)
+	if response.status_code == 200:
+		soup = BeautifulSoup(response.text, 'html.parser')
+		otvet = soup.find('p', id='otvev')
+		if otvet:
+			otvet_text = otvet.text
+			return(otvet_text)
+		else:
+			print("Тег <p id='otvet'> не найден")
+			return 0
+	else:
+		print("Ошибка при выполнении запроса")
+
 while True:
 	
 	value=read_file(patchtemp)
 	time.sleep(1)
-	if int(value)>25:
+	flag=chek_stat()
+	if flag=='2':
+		if int(value)>25:
+			print('avtoOn')
+			save_temp_to_file(2,patchstat)
+			time.sleep(1)	
+		else:
+			save_temp_to_file(3,patchstat)
+			print('avtoOff')
+			time.sleep(1)
+	elif flag=='1':
+		print('on')
 		save_temp_to_file(1,patchstat)
 		time.sleep(1)
 	else:
+		print('off')
 		save_temp_to_file(0,patchstat)
 		time.sleep(1)
 	stat=stat_file(patchstat)
@@ -35,4 +63,5 @@ while True:
 	response=requests.get(url)
 	print('Time otpr:'+str(datetime.datetime.now()))
 	print(url)
+	
 	time.sleep(30)
